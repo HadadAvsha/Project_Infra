689f8ed28be6d1ff22c08f49ff7a93b3c365838f
Avshalom Hadad <hadadavsha@gmail.com>
2022-11-13 18:29:34 +0200

    around the world

---
+++ argo-cd.tf
@@ -0,0 +1,32 @@
+provider "helm" {
+  kubernetes {
+    host                   = module.EKS.host #var.host
+    cluster_ca_certificate = base64decode(module.EKS.cluster_cert)#var.cluster_cert
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", module.EKS.cluster_name]#["eks", "get-token", "--cluster-name", var.cluster_name]
+      command     = "aws"
+    }
+      # depends_on   = [var.EKS.cluster_name]
+  }
+}
+
+# resource "kubernetes_namespace" "argocd" {
+#   metadata {
+#     name = "argo-cd"
+#   }
+# }
+
+resource "helm_release" "argocd" {
+  depends_on = [kubernetes_namespace.argocd]
+  name       = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  namespace  = "argo-cd"
+  version    = "5.8.3"
+
+  values = [
+    # "${file("argo-cdValues.yml")}"
+    file("${path.module}/argo-cd-values.yaml")
+  ]
+}
---
+++ ebs-csi-driver.tf
@@ -0,0 +1,8 @@
+resource "helm_release" "aws-ebs-csi-driver" {
+  name         = "aws-ebs-csi-driver"
+  repository   = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
+  chart        = "aws-ebs-csi-driver"
+  namespace    = "kube-system"
+  # force_update = true
+  depends_on   = [module.eks.node_group]#[var.node_group]
+}
